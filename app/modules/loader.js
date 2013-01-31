e1955582ddcec783ca0ad2ff2050a1d4b6349773
Joseph Bergen <jbergen@gmail.com>
2013-01-31 14:24:32 -0500

    added loading spinner

--- app/modules/loader.js
+++ app/modules/loader.js
@@ -1,10 +1,11 @@
 define([
     "app",
     // Libs
-    "backbone"
+    "backbone",
+    "libs/spin"
 ],
 
-function( app, Backbone ) {
+function( app, Backbone, Spinner ) {
 
     return Backbone.View.extend({
 
@@ -40,9 +41,26 @@ function( app, Backbone ) {
                     "background-size": "cover"
                 });
             }
+
+
+            this.spinner = new Spinner({
+                lines: 12, // The number of lines to draw
+                length: 20, // The length of each line
+                width: 5, // The line thickness
+                radius: 25, // The radius of the inner circle
+                corners: 1, // Corner roundness (0..1)
+                color: '#fff', // #rgb or #rrggbb
+                speed: 1, // Rounds per second
+                trail: 60, // Afterglow percentage
+                shadow: true, // Whether to render a shadow
+                hwaccel: false, // Whether to use hardware acceleration
+                className: 'spinner', // The CSS class to assign to the spinner
+                zIndex: 2e9 // The z-index (defaults to 2000000000)
+            }).spin( this.el );
         },
 
         onCanPlay: function() {
+            this.spinner.spin(false);
             this.$(".mobile-play").fadeIn();
         },
 
