d70a9add834296cae8f9f9f5cd8b83805ffbbea2
James Burns <jameburn@gmail.com>
2013-06-18 21:37:39 -0700

    add spinner to app

--- app/app.js
+++ app/app.js
@@ -28,6 +28,24 @@ function( $, _, Backbone, State, Spinner ) {
 
         metadata: $("meta[name=zeega]").data(),
 
+        spinner: new Spinner({
+            lines: 13, // The number of lines to draw
+            length: 7, // The length of each line
+            width: 4, // The line thickness
+            radius: 20, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            color: '#fff', // #rgb or #rrggbb
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 100, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+        }),
+
       /*
         app.state stores information on the current state of the application
       */
