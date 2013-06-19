666130788e673e280c80d2a6028edcdbb87ef0ae
jbergen <jbergen@gmail.com>
2013-06-18 19:04:44 -0700

    updated repos

--- app/app.js
+++ app/app.js
@@ -5,11 +5,13 @@ define([
     "backbone",
 
     "modules/state",
+    "engineVendor/spin",
+
     // Plugins.
     "plugins/backbone.layoutmanager"
 ],
 
-function( $, _, Backbone, State ) {
+function( $, _, Backbone, State, Spinner ) {
 
     var meta = $("meta[name=zeega]");
 
@@ -34,6 +36,26 @@ function( $, _, Backbone, State ) {
         Backbone: Backbone,
         $: $
     };
+
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 10, // The length of each line
+        width: 4, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#FFF', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+    app.spinner = new Spinner(opts);
 
     app.state = new State({ app: app });
 
