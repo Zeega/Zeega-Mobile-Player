deb3fb41356bee03a072c378e934a103480525e8
jbergen <jbergen@gmail.com>
2013-09-13 11:31:06 -0700

    rm logs

--- app/modules/remix-flash.js
+++ app/modules/remix-flash.js
@@ -10,18 +10,15 @@ function(app, Backbone) {
 
         visible: false,
         timer: null,
-        seen: [],
 
         template: "app/templates/remix-flash",
         className: "ZEEGA-remix-flash",
 
         initialize: function() {
             this.model.on("project:project_switch", this.onProjectSwitch, this );
-            // this.model.on("all", function(e, o){console.log("E",e, o)} );
         },
 
         serialize: function() {
-            console.log("SER:", this.model.zeega.getRemixData(), this.model.zeega.getCurrentProject() )
             return _.extend({
                 currentProject: this.model.zeega.getCurrentProject().toJSON(),
                 remixData: this.model.zeega.getRemixData()
@@ -34,13 +31,6 @@ function(app, Backbone) {
         },
 
         show: function(){
-
-            // if ( _.contains( this.seen, this.model.zeega.getCurrentProject().id ) ) {
-            //     this.$(".banner").addClass("seen");
-            // } else {
-            //     this.$(".banner").removeClass("seen");
-            // }
-
             this.clearTimer();
             this.model.off("page:focus");
 
@@ -49,7 +39,6 @@ function(app, Backbone) {
             this.timer = setTimeout(function() { this.hide(); }.bind(this), 3000 );
             this.visible = true;
             this.$(".banner").addClass("show");
-            this.seen.push( this.model.zeega.getCurrentProject().id );
         },
 
         waitForNext: function( mod, e, o ) {
